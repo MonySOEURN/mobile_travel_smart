e45cdcea8c5d3ff84869b3a43a2fa910af5d56a8
Mony SOEURN <soeurn.mony@gmail.com>
2020-01-09 15:30:20 +0700

    [TASK] Complete detail screen for static data

--- lib/screen/place_detail_screen.dart
+++ lib/screen/place_detail_screen.dart
@@ -32,12 +32,117 @@ class _State extends State<PlaceDetailScreen> {
   }
 
   Widget _buildBodyWidget(){
-    return Column(
+
+    // place photo and name
+    final placeInfo = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: <Widget>[
-        Image.network(widget.place.placeImage),
-        Text(widget.place.placeName),
+        Text(
+          widget.place.placeName,
+          style: TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
         Text(widget.place.placeLocation),
         Text("${widget.place.placeRate}"),
+      ],
+    );
+
+    // component for save button
+    final saveButton = Center(
+      child: Row(
+        children: <Widget>[
+          RaisedButton.icon(
+            onPressed: null,
+            icon: Icon(
+              Icons.flag,
+              color: Colors.black,
+            ),
+            label: Text(
+              "Save Place",
+              style: TextStyle(
+                color: Colors.black
+              ),
+            )
+          ),
+        ],
+      ),
+    );
+
+    // description variable
+    final description = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        Text(
+          "Description: ",
+          style: TextStyle(
+              fontWeight: FontWeight.bold,
+              fontSize: 16
+          ),
+        ),
+        SizedBox(height: 5,),
+        Text(
+            "Description Text area"
+        )
+      ],
+    );
+
+    // place time and scription detail
+    final placeTime = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        _buildTimeList("Time: ", "Open (8:00 AM - 5:00 PM)"),
+        _buildTimeList("Suggest Time: ", "(9:00 AM , 2:00 PM)"),
+        _buildTimeList("Time should spend: ", " 1 hour "),
+        SizedBox(height: 15,),
+      ],
+    );
+
+    // column between place info. Vs Time
+    final infoandTime = Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: <Widget>[
+        placeInfo,
+        SizedBox(height: 15,),
+        placeTime
+      ],
+    );
+
+    // row between place info., time detail Vs save button
+    final infoandButton = Row(
+      children: <Widget>[
+        infoandTime,
+        Spacer(),
+        saveButton,
+      ],
+    );
+
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+//          SizedBox(height: 4,),
+          Image.network(widget.place.placeImage),
+          infoandButton,
+          description,
+        ],
+      ),
+    );
+  }
+
+  Widget _buildTimeList(String key, String value){
+    return Row(
+      children: <Widget>[
+        Text(key),
+        Text(
+          value,
+          style: TextStyle(
+              fontSize: 10,
+              color: Colors.lightBlue
+          ),
+        )
       ],
     );
   }
